6164a1582c52fe26f4135bc12c543a407223a783
qhan1028 <michael.841028@gmail.com>
2017-04-25 02:25:36 +0800

    add csv to npz converter

---
+++ hw3/csv_to_np.py
@@ -0,0 +1,45 @@
+# ML 2017 hw3 transform data from csv to numpy
+
+import numpy as np
+import csv
+from sys import argv
+from keras.utils import np_utils
+
+SHAPE = 48
+CATEGORY = 7
+
+def read_train(filename):
+
+	X, Y = [], []
+	with open(filename, "r", encoding="big5") as f:
+		count = 0
+		for line in list(csv.reader(f))[1:]:
+			Y.append( float(line[0]) )
+			X.append( [float(x) for x in line[1].split()] )
+			count += 1
+			print("\rX_train: " + repr(count), end="", flush=True)
+		print("", flush=True)
+
+	return np.array(X), np_utils.to_categorical(Y, CATEGORY)
+
+def read_test(filename):
+
+	data = []
+	with open(filename, "r", encoding="big5") as f:
+		count = 0
+		for line in list(csv.reader(f))[1:]:
+			data.append( [float(x) for x in line[1].split()] )
+			count += 1
+			print("\rX_test: " + repr(count), end="", flush=True)
+		print("", flush=True)
+
+	return np.array(data)
+
+# argv: [1]train.csv [2]test.csv
+if __name__ == '__main__':
+	print("read train data...")
+	X_train, Y_train = read_train(argv[1])
+	print("read test data...")
+	X_test = read_test(argv[2])
+	print("save as npz...")
+	np.savez("data.npz", X_train, Y_train, X_test)
