c4f034be4aa5f0e4afcbb430f2219401d4089f66
qhan1028 <michael.841028@gmail.com>
2017-06-07 02:19:21 +0800

    finish ensemble: [0.83358]

--- hw6/ensemble.py
+++ hw6/ensemble.py
@@ -17,12 +17,12 @@ OUTPUT_FILE = sys.argv[2]
 MODEL_LIST = sys.argv[3]
 
 def read_list(filename):
-    
     weights, names = [], []
     with open(filename, 'r') as f:
         for line in f:
-            weights.append(float(line[0]))
-            names.append(line[1])
+            row = line.split()
+            weights.append(float(row[0]))
+            names.append(row[1])
 
     return weights, names
 
@@ -55,12 +55,13 @@ def main():
     print('Predict')
     def rmse(y_true, y_pred): return K.sqrt( K.mean((y_pred - y_true)**2) )
 
-    sum_result = np.zeros(len(test))
-    sum_weight = 0
+    sum_result = np.zeros((len(test), 1))
+    sum_weights = 0
     for w, name in zip(weights, names):
         
-        print('weight: %f' % w + ', name:' + name)
-        model = load_model(MODEL_DIR + '/' + name)
+        print('weight: %f' % w + ', name: ' + name, end=' ', flush=True)
+        model = load_model(MODEL_DIR + '/' + name, custom_objects={'rmse': rmse})
+        print('[loaded]', end=' ', flush=True)
         result = np.zeros(len(test))
         if name[:2] == 'mf':
             if name[3:9] == 'simple':
@@ -72,7 +73,8 @@ def main():
                 result = model.predict([userID, movieID])
             else:
                 result = model.predict([userID, movieID, userGender, userAge, userOccu, movieGenre])
-                
+        print('[predicted]', flush=True)
+
         sum_result += result * w
         sum_weights += w
     
---
+++ hw6/hw6_best.sh
@@ -0,0 +1,3 @@
+wget https://www.dropbox.com/s/99qonlvcyfr4id7/ml2017_hw6_model.zip
+unzip -u ml2017_hw6_model.zip -d model
+python ensemble.py $1 $2 model_list.txt
