9951c870e8458ef777a596005cc897a6822989bf
qhan1028 <michael.841028@gmail.com>
2017-06-08 02:20:27 +0800

    hw3, clean up

--- hw3/cnn_test.py
+++ hw3/cnn_test.py
@@ -12,52 +12,57 @@ np.set_printoptions(precision = 6, suppress = True)
 READ_FROM_NPZ = 0
 SHAPE = 48
 
+DATA_DIR = './data'
+
 def read_test(filename):
 
 	data = []
-	with open(filename, "r", encoding="big5") as f:
+	with open(filename, 'r', encoding='big5') as f:
 		count = 0
 		for line in list(csv.reader(f))[1:]:
 			data.append( [float(x) for x in line[1].split()] )
 			count += 1
-			print("\rX_test: " + repr(count), end="", flush=True)
-		print("", flush=True)
+			print('\rX_test: ' + repr(count), end='', flush=True)
+		print('', flush=True)
 
 	return np.array(data)
 
 def write_file(filename, result):
 
-	with open(filename, "w", encoding="big5") as f:
+	with open(filename, 'w', encoding='big5') as f:
 		
-		f.write("id,label\n")
+		f.write('id,label\n')
 		for i in range(len(result)):
 			predict = np.argmax(result[i])
-			f.write(repr(i) + "," + repr(predict) + "\n")
+			f.write(repr(i) + ',' + repr(predict) + '\n')
 
 # argv: [1]test.csv [2]predict.csv [3]model.h5
 def main():
 	
+    print('============================================================')
 	X_test = []
 	if READ_FROM_NPZ:
-		print("read from npz...")
-		data = np.load("data.npz")
+		print('Read from npz')
+		data = np.load(DATA_DIR + '/data.npz')
 		X_test = data['arr_2']
 	else:
-		print("read test data...")
+		print('Read test data')
 		X_test = read_test(argv[1])
 
-	print("reshape data...")
+	print('Reshape data')
 	X_test = X_test/255
 	X_test = X_test.reshape(X_test.shape[0], SHAPE, SHAPE, 1)
 
-	print("load model...")
+    print('============================================================')
+	print('Load model')
 	model = load_model(argv[3])
 
-	print("predict...")
+	print('Predict')
 	result = model.predict(X_test, batch_size = 128, verbose = 1)
 
-	print("output result...", flush=True)
+    print('============================================================')
+	print('Output result', flush=True)
 	write_file(argv[2], result)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 	main()
--- hw3/cnn_train.py
+++ hw3/cnn_train.py
@@ -19,109 +19,126 @@ from keras.callbacks import EarlyStopping, ModelCheckpoint
 SHAPE = 48
 CATEGORY = 7
 
-READ_FROM_NPZ = 0
-CHECK_POINT = 0
-SAVE_HISTORY = 0
+DATA_DIR = './data'
+MODEL_DIR = './model'
+HIS_DIR = './history'
+
+READ_FROM_NPZ = 1
 AUGMENT = 1
 
 def read_train(filename):
 
-	X, Y = [], []
-	with open(filename, "r", encoding="big5") as f:
-		count = 0
-		for line in list(csv.reader(f))[1:]:
-			Y.append( float(line[0]) )
-			X.append( [float(x) for x in line[1].split()] )
-			count += 1
-			print("\rX_train: " + repr(count), end="", flush=True)
-		print("", flush=True)
+    X, Y = [], []
+    with open(filename, 'r', encoding='big5') as f:
+        count = 0
+        for line in list(csv.reader(f))[1:]:
+            Y.append( float(line[0]) )
+            X.append( [float(x) for x in line[1].split()] )
+            count += 1
+            print('\rX_train: ' + repr(count), end='', flush=True)
+        print('', flush=True)
 
-	return np.array(X), np_utils.to_categorical(Y, CATEGORY)
+    return np.array(X), np_utils.to_categorical(Y, CATEGORY)
 
 # argv: [1]train.csv
 def main():
 
-	X, Y = [], []
-	if READ_FROM_NPZ:
-		print("read from npz...")
-		data = np.load("data.npz")
-		X = data['arr_0']
-		Y = data['arr_1']
-	else:
-		print("read train data...")
-		X, Y = read_train(argv[1])
+    print('============================================================')
+    X, Y = [], []
+    if READ_FROM_NPZ:
+        print('Read from npz')
+        data = np.load(DATA_DIR + '/data.npz')
+        X = data['arr_0']
+        Y = data['arr_1']
+    else:
+        print('Read train data')
+        X, Y = read_train(argv[1])
 
-	print("reshape data...")
-	X = X/255
-	X = X.reshape(X.shape[0], SHAPE, SHAPE, 1)
+    print('Reshape data')
+    X = X/255
+    X = X.reshape(X.shape[0], SHAPE, SHAPE, 1)
 
-	print("construct model...")
-	model = Sequential()
-	model.add(Conv2D(32, (3, 3), input_shape=(48, 48, 1), activation='relu', padding='same'))
-	model.add(BatchNormalization(axis=-1))
-	model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
-	model.add(BatchNormalization(axis=-1))
-	model.add(MaxPooling2D((2, 2)))
+    print('============================================================')
+    print('Construct model')
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), input_shape=(48, 48, 1), activation='relu', padding='same'))
+    model.add(BatchNormalization(axis=-1))
+    model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+    model.add(BatchNormalization(axis=-1))
+    model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+    model.add(BatchNormalization(axis=-1))
+    model.add(MaxPooling2D((2, 2)))
 
-	model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
-	model.add(BatchNormalization(axis=-1))
-	model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
-	model.add(BatchNormalization(axis=-1))
-	model.add(MaxPooling2D((2, 2)))
+    model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+    model.add(BatchNormalization(axis=-1))
+    model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+    model.add(BatchNormalization(axis=-1))
+    model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+    model.add(BatchNormalization(axis=-1))
+    model.add(MaxPooling2D((2, 2)))
 
-	model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
-	model.add(BatchNormalization(axis=-1))
-	model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
-	model.add(BatchNormalization(axis=-1))
-	model.add(MaxPooling2D((2, 2)))
+    model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+    model.add(BatchNormalization(axis=-1))
+    model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+    model.add(BatchNormalization(axis=-1))
+    model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+    model.add(BatchNormalization(axis=-1))
+    model.add(MaxPooling2D((2, 2)))
 
-	model.add(Flatten())
-	model.add(Dense(units = 256, activation='relu'))
-	model.add(Dense(units = 128, activation='relu'))
-	model.add(Dense(units = 7, activation='softmax'))
-	model.summary()
+    model.add(Flatten())
+    model.add(Dense(units = 256, activation='relu'))
+    model.add(Dense(units = 128, activation='relu'))
+    model.add(Dense(units = 64, activation='relu'))
+    model.add(Dense(units = 7, activation='softmax'))
+    model.summary()
 
-	print("compile model...")
-	model.compile(loss='categorical_crossentropy',optimizer="adam",metrics=['accuracy'])
+    print('Compile model')
+    model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
 
-	VAL = 2400
-	BATCH = 128
-	EPOCHS = 30
-	score = [0]
-	if AUGMENT == 1: 
-		print("train with augmented data...")
-		datagen = ImageDataGenerator(vertical_flip=False, horizontal_flip=True, fill_mode='nearest', \
-																 height_shift_range=0.1, width_shift_range=0.1, rotation_range=20.)
-		Xv = X[:VAL]
-		Yv = Y[:VAL]
-		datagen.fit(X[VAL:], seed=1028)
-		history = []
-		if CHECK_POINT:
-			os.mkdir("new_model")
-			filepath = "./new_model/{epoch:02d}_{val_acc:.6f}.h5"
-			cp = ModelCheckpoint(filepath, verbose=1)
-			history = model.fit_generator(datagen.flow(X[VAL:], Y[VAL:], batch_size=BATCH, seed=1028), callbacks=[cp],\
-										  samples_per_epoch=len(X[VAL:]), epochs=EPOCHS, verbose=1, validation_data=(Xv, Yv))
-			os.rename("new_model", "{:.6f}".format(history.history['val_acc'][-1]))
-		else:
-			history = model.fit_generator(datagen.flow(X[VAL:], Y[VAL:], batch_size=BATCH, seed=1028),\
-										  samples_per_epoch=len(X[VAL:]), epochs=EPOCHS, verbose=1, validation_data=(Xv, Yv))
-		score.append(history.history['val_acc'][-1])
-		print("train accuracy (last) = " + "{:.6f}".format(score[1]))
-		if SAVE_HISTORY:
-			print("save history...")
-			h = history.history
-			np.savez("{:.6f}".format(score[1]) + "_history.npz", h['acc'], h['val_acc'])
-	else:
-		print("train with raw data...")
-		es = EarlyStopping(monitor='val_acc', patience=5, verbose=1, mode='auto')
-		model.fit(X, Y, batch_size=BATCH, epochs=EPOCHS, verbose=1, validation_split=0.1, callbacks=[es])
-		print("evaluate train...")
-		score = model.evaluate(X, Y)
-		print("train accuracy (all) = " + repr(score[1]))
+    print('============================================================')
+    VAL = 2400
+    BATCH = 128
+    EPOCHS = 400
+    if AUGMENT == 1: 
+        print('Train with augmented data')
+        datagen = ImageDataGenerator(vertical_flip=False, horizontal_flip=True, fill_mode='nearest', \
+                                     height_shift_range=0.1, width_shift_range=0.1, rotation_range=20.)
+        Xv = X[:VAL]
+        Yv = Y[:VAL]
+        datagen.fit(X[VAL:], seed=1019)
+        cp = ModelCheckpoint('temp_model.h5', monitor='val_acc', verbose=0, save_best_only=True)
+        history = model.fit_generator(datagen.flow(X[VAL:], Y[VAL:], batch_size=BATCH, seed=1019), callbacks=[cp],\
+                                      samples_per_epoch=len(X[VAL:]), epochs=EPOCHS, verbose=1, validation_data=(Xv, Yv))
 
-	print("save model...")
-	model.save("{:.6f}".format(score[1]) + ".h5")
+        print('============================================================')
+        H = history.history
+        best_val = '{:.6f}'.format(np.max(H['val_acc']))
+        last_val = '{:.6f}'.format(H['val_acc'][-1])
+        print('Best val: ' + best_val)
+        print('Last val: ' + last_val)
+
+        print('============================================================')
+        print('Save best model')
+        os.rename('temp_model.h5', MODEL_DIR + '/' + best_val + '.h5')
+        print('Save last model')
+        model.save(MODEL_DIR + '/' + last_val + '.h5')
+        print('Save history')
+        np.savez(HIS_DIR + '/' + best_val + '_history.npz', acc=H['acc'], val_acc=H['val_acc'])
+    else:
+        print('Train with raw data')
+        es = EarlyStopping(monitor='val_acc', patience=5, verbose=1, mode='auto')
+        model.fit(X, Y, batch_size=BATCH, epochs=EPOCHS, verbose=1, validation_split=0.1, callbacks=[es])
+
+        print('============================================================')
+        print('Evaluate train')
+        score = model.evaluate(X, Y)
+        score = '{:.6f}'.format(score[1])
+        print('Train accuracy (all):', score)
+
+        print('============================================================')
+        print('Save model')
+        model.save(MODEL_DIR + '/' + score + '.h5')
+
 
 if __name__ == '__main__':
-	main()
+    main()
