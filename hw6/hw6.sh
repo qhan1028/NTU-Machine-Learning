0b5072df119b81df52978a9826e45443fcfae7ad
qhan1028 <michael.841028@gmail.com>
2017-06-09 17:58:55 +0800

    hw6, fix sh

--- hw6/hw6.sh
+++ hw6/hw6.sh
@@ -1,3 +1,3 @@
 wget https://www.dropbox.com/s/6g4x3w5b3yzpqbk/ml2017_hw6.zip
-unzip -u ml2017_hw6.zip -d model
-python3.5 mf_test.py $1 $2 ./model/mf_0.839977.h5
+unzip -u ml2017_hw6.zip -d simple_model
+python3.5 mf_test.py $1 $2 ./simple_model/mf_0.839977.h5
